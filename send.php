56594315a94540390a6c17c4c17a0cba1b4ad838
Gittolk <gittolk@mail.ru>
2020-07-31 16:34:03 +0300

    create: send message and subscribe form

---
+++ send.php
@@ -0,0 +1,70 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$phone = $_POST['phone'];
+
+// Формирование самого письма
+if(!empty($email)){
+    $title = "Новый подписчик";
+$body = "
+<h2>Желает подписаться</h2>
+<b>Email:</b> $email<br>
+";}
+
+else {
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'gittolk@mail.ru'; // Логин на почте
+    $mail->Password   = '3001117a'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('gittolk@mail.ru', 'Артем Сергеевич'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('arttolk@bk.ru');  
+
+
+    
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+if(empty($email)){
+header('location: thankyou.html');}
+else{(header('location: thankssubscribe.html'));}
